ec492c2bdb28c111ba5f47c95da6c89c3f082c64
wardr8 <34200753+wardr8@users.noreply.github.com>
2018-09-22 20:46:06 -0400

    Update main.c

--- 2.2 to 2.4/main.c
+++ 2.2 to 2.4/main.c
@@ -87,13 +87,6 @@ long map(long x, long in_min, long in_max, long out_min, long out_max)
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-int getGroupNumber(int inputADCval, int ADCmax, int ADCmin, int groupMAX, int groupMIN)
-{
-	int band=0;
-	band = (ADCmax-ADCmin+1)/(groupMAX-groupMIN+1);
-	return ((groupMAX-groupMIN)-((inputADCval-ADCmin)/band));
-}
-
 int main(void)
 {
 	uart_init();
@@ -102,10 +95,6 @@ int main(void)
 	set(DDRB, 1);		//set PB1 to output
 	clear(DDRB, 0);		//set PB0 as input
 	set(DDRD, 6);		//set PD6 as output
-	clear(DDRC, 0);		//set C0 as input
-	set(DDRB, 2);		//set PB2, 3, 4 to output
-	set(DDRB, 3);
-	set(DDRB, 4);
 	
 	//-------Setup Timer 1-------//
 	clear(TCCR1B, CS10);	//clock source on, /8 prescale
@@ -125,30 +114,6 @@ int main(void)
 	//-------Setup timer 2-------//
 	TCCR0A = 0x42;	//0100 0010 toggle OC0A on match, CTC mode up to OCR0A
 	TCCR0B = 0x03;	//0000 0011 /64 prescale
-	
-	//-------ADC setup-------//
-	set(ADMUX, REFS0);	//setting reference voltage for ADC to Vcc
-	clear(ADMUX, REFS1);
-		
-	set(ADCSRA, ADPS2);	//set ADC prescaler to 125 kHz based on system clock
-	set(ADCSRA, ADPS1);
-	set(ADCSRA, ADPS0);
-		
-	clear(ADCSRB, ADTS2);	//free running mode
-	clear(ADCSRB, ADTS1);
-	clear(ADCSRB, ADTS0);
-		
-	set(ADCSRA, ADATE);	//auto trigger (bc free running)
-		
-	set(DIDR0, ADC0D);	//disable digital input for ADC0
-		
-	clear(ADMUX, MUX3);	//ADC0 - pin C0
-	clear(ADMUX, MUX2);
-	clear(ADMUX, MUX1);
-	clear(ADMUX, MUX0);
-		
-	set(ADCSRA, ADEN);	//enable ADC subsystem
-	set(ADCSRA, ADSC);	//begin conversion
 
 	//-----//
 	clear(PORTB, 1);
@@ -170,7 +135,8 @@ int main(void)
 			}
 			val = map(ticks, 400, 15000, 61, 123);
 			//printf("val =  %u \n", val);
-			if (val < 68){
+		
+			if (val < 68){			//used for discrete output
 				OCR0A = 61;	//
 			}
 			else if (val >= 68 && val < 76){
@@ -196,53 +162,7 @@ int main(void)
 			}
 			
 			//OCR0A = val;		//used for continuous output
-			
-			//printf("ADC = %i \n", ADC);
-			int number = 7 - getGroupNumber(ADC, 920, 500, 7, 0);
-			if (number > 7){
-				number = 7;
-			}
-			//printf("group number = %i \n", number);
-			if (number == 0){
-				clear(PORTB, 2);
-				clear(PORTB, 3);
-				clear(PORTB, 4);
-			}
-			else if (number == 1){
-				set(PORTB, 2);
-				clear(PORTB, 3);
-				clear(PORTB, 4);
-			}
-			else if (number == 2){
-				clear(PORTB, 2);
-				set(PORTB, 3);
-				clear(PORTB, 4);
-			}
-			else if (number == 3){
-				set(PORTB, 2);
-				set(PORTB, 3);
-				clear(PORTB, 4);
-			}
-			else if (number == 4){
-				clear(PORTB, 2);
-				clear(PORTB, 3);
-				set(PORTB, 4);
-			}
-			else if (number == 5){
-				set(PORTB, 2);
-				clear(PORTB, 3);
-				set(PORTB, 4);
-			}
-			else if (number == 6){
-				clear(PORTB, 2);
-				set(PORTB, 3);
-				set(PORTB, 4);
-			}
-			else if (number == 7){
-				set(PORTB, 2);
-				set(PORTB, 3);
-				set(PORTB, 4);
-			}
+
 			
 		}
 		return(0);
